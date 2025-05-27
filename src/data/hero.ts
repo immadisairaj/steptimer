1c8a0ff5e033329e64983e02c97261cf133c3534
immadisairaj <immadirajendra.sai@gmail.com>
2025-05-27 23:34:43 +0530

    change mockup screen

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Step-by-Step Timer, Auto Play, Sequencial',
     subheading: 'Ease with stopping and starting timers for continuous works',
-    centerImageSrc: '/images/hero-mockup.webp',
+    centerImageSrc: '/images/mockup.png',
 }
