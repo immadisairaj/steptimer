bbfaf381100ed46e34bdeae3483aba69ba1fad26
immadisairaj <immadirajendra.sai@gmail.com>
2026-03-05 20:18:35 +0530

    add appstore link

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -1,6 +1,6 @@
 export const ctaDetails = {
     heading: 'Step Timer',
     subheading: 'Sequential Timers Made Simple - download now and start your journey with Step Timer.',
-    appStoreUrl: '#',
+    appStoreUrl: 'https://apps.apple.com/app/6743447841',
     googlePlayUrl: 'https://play.google.com/store/apps/details?id=dev.immadisairaj.step_timer'
 }
